71016e68a13569907bac724d2a47ca949908abc5
Odailson Silva <dev.odailson2021@gmail.com>
2023-07-10 13:47:14 -0300

    feat: hysky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        "react-app",
-        "prettier",
-        'airbnb-base'
-    ],
-    plugins: ["react-hooks"],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    rules: {
-        'class-methods-use-this': 'off',
-        'consistent-return': 'off',
-        camelcase: 'off',
-        'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-        'import/no-unresolved': 'error',
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
-    },
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'prettier',
+    'airbnb-base',
+  ],
+  plugins: ['react-hooks'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+  },
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
-import { extendBaseTheme } from "@chakra-ui/react";
+import { extendBaseTheme } from '@chakra-ui/react';
 
-export const theme = extendBaseTheme({})
+export default extendBaseTheme({
+  component: {},
+});
